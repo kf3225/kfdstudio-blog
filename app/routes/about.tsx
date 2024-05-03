75e7dff201cb610d4b238e80a6a60f6eb93ce8c5
kf3225 <k.fujita3225@gmail.com>
2024-05-03 23:51:43 +0900

    format about page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,17 +1,48 @@
 export default function Abount() {
-    return <div>
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate provident minima commodi, cupiditate soluta, inventore maiores explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
-	</div>
+	return (
+		<div>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus
+			voluptate provident minima commodi, cupiditate soluta, inventore maiores
+			explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus
+			quaerat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
+			temporibus voluptate provident minima commodi, cupiditate soluta,
+			inventore maiores explicabo illum est porro vitae odit. Dolorem veniam
+			praesentium nam natus quaerat. Lorem ipsum, dolor sit amet consectetur
+			adipisicing elit. Fuga temporibus voluptate provident minima commodi,
+			cupiditate soluta, inventore maiores explicabo illum est porro vitae odit.
+			Dolorem veniam praesentium nam natus quaerat. Lorem ipsum, dolor sit amet
+			consectetur adipisicing elit. Fuga temporibus voluptate provident minima
+			commodi, cupiditate soluta, inventore maiores explicabo illum est porro
+			vitae odit. Dolorem veniam praesentium nam natus quaerat. Lorem ipsum,
+			dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate
+			provident minima commodi, cupiditate soluta, inventore maiores explicabo
+			illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus
+			voluptate provident minima commodi, cupiditate soluta, inventore maiores
+			explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus
+			quaerat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
+			temporibus voluptate provident minima commodi, cupiditate soluta,
+			inventore maiores explicabo illum est porro vitae odit. Dolorem veniam
+			praesentium nam natus quaerat. Lorem ipsum, dolor sit amet consectetur
+			adipisicing elit. Fuga temporibus voluptate provident minima commodi,
+			cupiditate soluta, inventore maiores explicabo illum est porro vitae odit.
+			Dolorem veniam praesentium nam natus quaerat. Lorem ipsum, dolor sit amet
+			consectetur adipisicing elit. Fuga temporibus voluptate provident minima
+			commodi, cupiditate soluta, inventore maiores explicabo illum est porro
+			vitae odit. Dolorem veniam praesentium nam natus quaerat. Lorem ipsum,
+			dolor sit amet consectetur adipisicing elit. Fuga temporibus voluptate
+			provident minima commodi, cupiditate soluta, inventore maiores explicabo
+			illum est porro vitae odit. Dolorem veniam praesentium nam natus quaerat.
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga temporibus
+			voluptate provident minima commodi, cupiditate soluta, inventore maiores
+			explicabo illum est porro vitae odit. Dolorem veniam praesentium nam natus
+			quaerat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
+			temporibus voluptate provident minima commodi, cupiditate soluta,
+			inventore maiores explicabo illum est porro vitae odit. Dolorem veniam
+			praesentium nam natus quaerat. Lorem ipsum, dolor sit amet consectetur
+			adipisicing elit. Fuga temporibus voluptate provident minima commodi,
+			cupiditate soluta, inventore maiores explicabo illum est porro vitae odit.
+			Dolorem veniam praesentium nam natus quaerat.
+		</div>
+	);
 }
