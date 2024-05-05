945950c6333dc8d6bb2238d5cfa10c921fa72f5f
kf3225 <k.fujita3225@gmail.com>
2024-05-05 17:45:59 +0900

    add each post page layout

---
+++ app/routes/posts.$slug.tsx
@@ -0,0 +1,11 @@
+import { useParams } from "@remix-run/react";
+import { posts } from "../utils/posts";
+
+export default function Post() {
+	const params = useParams();
+	const [post, ..._] = Object.entries(posts)
+		.filter(([filename, _]) => `${params.slug}.mdx` === filename)
+		.map(([_, meta]) => meta);
+	const Component = post.default;
+	return <Component />;
+}
