3e1ec37e2562af3197ce8ffb2cfad98cdea04b63
kf3225 <k.fujita3225@gmail.com>
2024-05-05 23:43:01 +0900

    format

--- app/root.tsx
+++ app/root.tsx
@@ -1,8 +1,8 @@
 import clsx from "clsx";
 import {
-	PreventFlashOnWrongTheme,
-	ThemeProvider,
-	useTheme,
+  PreventFlashOnWrongTheme,
+  ThemeProvider,
+  useTheme,
 } from "remix-themes";
 
 import { cssBundleHref } from "@remix-run/css-bundle";
@@ -11,13 +11,13 @@ import { themeSessionResolver } from "./sessions.server";
 
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/cloudflare";
 import {
-	Links,
-	LiveReload,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-	useLoaderData,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
 import styles from "~/tailwind.css?url";
@@ -25,51 +25,51 @@ import { Footer } from "./components/footer";
 import { Header } from "./components/header";
 
 export const links: LinksFunction = () => [
-	{ rel: "stylesheet", href: styles },
-	...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: styles },
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 // Return the theme from the session storage using the loader
 export async function loader({ request }: LoaderFunctionArgs) {
-	const { getTheme } = await themeSessionResolver(request);
-	return {
-		theme: getTheme(),
-	};
+  const { getTheme } = await themeSessionResolver(request);
+  return {
+    theme: getTheme(),
+  };
 }
 // Wrap your app with ThemeProvider.
 // `specifiedTheme` is the stored theme in the session storage.
 // `themeAction` is the action name that's used to change the theme in the session storage.
 export default function AppWithProviders() {
-	const data = useLoaderData<typeof loader>();
-	return (
-		<ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
-			<App />
-		</ThemeProvider>
-	);
+  const data = useLoaderData<typeof loader>();
+  return (
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
+  );
 }
 
 export function App() {
-	const data = useLoaderData<typeof loader>();
-	const [theme] = useTheme();
-	return (
-		<html lang="ja" className={clsx(theme)}>
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Meta />
-				<PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
-				<Links />
-			</head>
-			<body className="min-h-dvh flex flex-col">
-				<Header />
-				<div className="container flex-1">
-					<Outlet />
-				</div>
-				<Footer />
-				<ScrollRestoration />
-				<Scripts />
-				<LiveReload />
-			</body>
-		</html>
-	);
+  const data = useLoaderData<typeof loader>();
+  const [theme] = useTheme();
+  return (
+    <html lang="ja" className={clsx(theme)}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
+        <Links />
+      </head>
+      <body className="min-h-dvh flex flex-col">
+        <Header />
+        <div className="container flex-1">
+          <Outlet />
+        </div>
+        <Footer />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,61 +2,61 @@ import { Link } from "@remix-run/react";
 import { Button } from "~/components/ui/button";
 import { posts } from "~/utils/posts";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "../components/ui/card";
 
 export default function Index() {
-	return (
-		<main className="flex justify-center">
-			<ul className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-				{Object.entries(posts)
-					.slice(0, 4)
-					.map(([path, meta]) => {
-						return (
-							<li key={path}>
-								<Card className="relative">
-									<CardHeader>
-										<div className="aspect-video bg-muted mb-4" />
-										<CardTitle>
-											<h3>{meta.frontmatter.title}</h3>
-											<Link
-												className="absolute inset-0"
-												to={`posts/${path.replace(".mdx", "")}`}
-											/>
-										</CardTitle>
-									</CardHeader>
-									<CardContent>
-										<CardDescription>
-											<p>{new Date(meta.frontmatter.pubDate).toISOString()}</p>
-											<p>{meta.frontmatter.description}</p>
-										</CardDescription>
-									</CardContent>
-									<CardFooter>
-										<ul className="flex gap-1">
-											{meta.frontmatter.tags.map((tag) => (
-												<li key={tag}>
-													<Button asChild variant={"secondary"} size={"sm"}>
-														<Link
-															to={`/tags/${tag}`}
-															className="text-[10px] text-muted-foreground"
-														>
-															{tag}
-														</Link>
-													</Button>
-												</li>
-											))}
-										</ul>
-									</CardFooter>
-								</Card>
-							</li>
-						);
-					})}
-			</ul>
-		</main>
-	);
+  return (
+    <main className="flex justify-center">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {Object.entries(posts)
+          .slice(0, 4)
+          .map(([path, meta]) => {
+            return (
+              <li key={path}>
+                <Card className="relative">
+                  <CardHeader>
+                    <div className="aspect-video bg-muted mb-4" />
+                    <CardTitle>
+                      <h3>{meta.frontmatter.title}</h3>
+                      <Link
+                        className="absolute inset-0"
+                        to={`posts/${path.replace(".mdx", "")}`}
+                      />
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <CardDescription>
+                      <p>{new Date(meta.frontmatter.pubDate).toISOString()}</p>
+                      <p>{meta.frontmatter.description}</p>
+                    </CardDescription>
+                  </CardContent>
+                  <CardFooter>
+                    <ul className="flex gap-1">
+                      {meta.frontmatter.tags.map((tag) => (
+                        <li key={tag}>
+                          <Button asChild variant={"secondary"} size={"sm"}>
+                            <Link
+                              to={`/tags/${tag}`}
+                              className="text-[10px] text-muted-foreground"
+                            >
+                              {tag}
+                            </Link>
+                          </Button>
+                        </li>
+                      ))}
+                    </ul>
+                  </CardFooter>
+                </Card>
+              </li>
+            );
+          })}
+      </ul>
+    </main>
+  );
 }
