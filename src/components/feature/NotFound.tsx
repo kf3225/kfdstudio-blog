12bca1af0dab1317e1c6ce2f2f4871ad31c0b094
kf3225 <k.fujita3225@gmail.com>
2024-10-18 15:43:18 +0900

    add not found page

---
+++ src/components/feature/NotFound.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const Introduction: Component = () => {
+  return (
+    <div class="mx-auto">
+      <p class="text-3xl font-bold">Page Not Found</p>
+    </div>
+  );
+};
+
+export default Introduction;
