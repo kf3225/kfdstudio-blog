7b65b6ffd7c4f5cbc084378baeb083acfa762c97
kf3225 <k.fujita3225@gmail.com>
2026-01-31 14:36:20 +0900

    chore: remove old resources (without docs)

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import type { Component, JSX } from "solid-js";
-import Layout from "./components/layout/Layout";
-
-const App: Component<{ children?: JSX.Element }> = ({ children }) => {
-  return <Layout>{children}</Layout>;
-};
-
-export default App;
